82ce278f94a6fbb2462b408386452e8f311bc4b1
Ramon-Cristian Marcu <ramondotmarcu@gmail.com>
2023-09-21 16:40:36 +0300

    Add files via upload

---
+++ breadShopTest.js
@@ -0,0 +1,13 @@
+const { expect } = require("chai");
+const { Builder } = require("selenium-webdriver");
+
+describe("First suite", function() {
+    this.timeout(5000);
+
+    it("First test", async function () {
+        let driver = await new Builder().forBrowser('chrome').build();
+        expect(1).to.equal(1);
+
+        await driver.quit();
+    });
+});
